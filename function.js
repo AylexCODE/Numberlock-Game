564ba415d103ec6d19d798b53350ea96068ac57a
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-20 08:19:20 +0800

    Added reset button when tiles are occupied

--- function.js
+++ function.js
@@ -2,6 +2,7 @@ const container = document.getElementById("container");
 const guessBoxes = document.getElementsByClassName("guessBox");
 const box = document.getElementById("box");
 const placeBtn = document.querySelector(".placeBtn");
+const resetBtn = document.querySelector(".resetBtn");
 
 let snapTo = [0, 0];
 let resetGuessBox;
@@ -9,21 +10,26 @@ let resetGuessBox;
 let currentNumber = rng();
 const numbers = [];
 
-for(let i = 0; i < 10; i++){
-    const guessBox = document.createElement("span");
-    guessBox.id = `guessBox${i}`;
-    guessBox.classList = "guessBox";
-    guessBox.innerHTML = `<p>${i+1}</p>`;
-    container.appendChild(guessBox);
-}
+let boxes = [];
+const dataBoxes = [];
+
+function start(){
+    for(let i = 0; i < 10; i++){
+        const guessBox = document.createElement("span");
+        guessBox.id = `guessBox${i}`;
+        guessBox.classList = "guessBox";
+        guessBox.innerHTML = `<p>${i+1}</p>`;
+        container.appendChild(guessBox);
+    }
+        
     
-let boxes = []; const dataBoxes = [];
-Array.from(guessBoxes).forEach((box, i) => {
-    boxes[i] = { x: box.getBoundingClientRect().x, y: box.getBoundingClientRect().y};
-    dataBoxes[i] = { x: box.getBoundingClientRect().x, y: box.getBoundingClientRect().y};
-});
-    
-box.innerHTML = `<h2>${currentNumber}</h2>`;
+    Array.from(guessBoxes).forEach((box, i) => {
+        boxes[i] = { x: box.getBoundingClientRect().x, y: box.getBoundingClientRect().y};
+        dataBoxes[i] = { x: box.getBoundingClientRect().x, y: box.getBoundingClientRect().y};
+    });
+        
+    box.innerHTML = `<h2>${currentNumber}</h2>`;
+} start();
 
 function rng(){
     return Math.floor(Math.random() * 100);
@@ -51,9 +57,23 @@ function lockIn(){
     });
     
     if(boxes.length == 0){
-        box.remove();
+        box.style.display = "none";
+        resetBtn.style.display = "grid";
+        placeBtn.style.display = "none";
         calc();
     }
+}
+
+function reset(){
+    resetGuessBox();
+    box.style.display = "grid";
+    
+    resetBtn.style.display = "none";
+    placeBtn.style.display = "block";
+    
+    numbers.length = 0;
+    container.replaceChildren();
+    start();
 }
 
 async function calc(){
