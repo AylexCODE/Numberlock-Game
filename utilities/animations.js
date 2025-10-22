f29b43587c6e87607b3bfd8ae8bad8f9ce7b618a
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-22 11:27:01 +0800

    UI improvements and implementing background music

---
+++ utilities/animations.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(Draggable);
+    
+    gsap.set("#boxContainer", {
+        x: window.innerWidth / 2 - 25,
+        y: window.innerHeight / 2 - 25 - 200
+    });
+
+    resetGuessBox = function resetGuessBoxPosition(){
+        gsap.set(box, {
+            x: window.innerWidth / 2 - 25,
+            y: window.innerHeight / 2 - 25 - 200
+        });
+    }
+    resetGuessBox();
+    document.getElementById("box").style.visibility = "visible";
+    document.getElementById("boxContainer").style.visibility = "visible";
+    
+    Draggable.create(box, {
+        type: "x,y",
+        inertia: true,
+        bounds: document.getElementsByTagName("body")[0],
+        liveSnap: {
+            points: function (point) {
+                buttonState(false);
+                
+                for(const p of boxes){
+                    const dx = point.x - p.x;
+                    const dy = point.y - p.y;
+                    if (Math.sqrt(dx * dx + dy * dy) < 30) {
+                        snapTo = [p.x, p.y];
+                        return point;
+                    }
+                }
+                snapTo = null;
+                return point;
+            }
+        },
+        onDragEnd: function(){
+            if(snapTo){
+                gsap.to("#box", {
+                    x: snapTo[0],
+                    y: snapTo[1],
+                    delay: 0.2,
+                    ease: "power2.inOut"
+                });
+                
+                setTimeout(() => {buttonState(true); playBoxSnapSound();}, 400);
+            }else{
+                buttonState(false);
+            }
+        }
+    });
+});
--- utilities/sounds.js
+++ utilities/sounds.js
@@ -1,7 +1,18 @@
+const bgmList = ["bgm_loop1", "bgm_loop2", "bgm_sorcerer_loop", "blues_guitar_loop", "jazzy_spy_loop", "soft_piano_loop", "tutorial_loop"];
+
+let bgm = new Audio(`../assets/bgm/${bgmList[Math.floor(Math.random() * 7)]}.ogg`);
 const lockIn_sound = new Audio("../assets/sounds/lock_in.ogg");
 const boxSnap_sound = new Audio("../assets/sounds/box_snap.ogg");
 const scoreResult_sound = new Audio("../assets/sounds/score_result.ogg");
 const btnDisabled_sound = new Audio("../assets/sounds/button_disabled.ogg");
+
+function BGMMusic(){
+    bgm.pause();
+    bgm.currentTime = 0;
+    bgm = new Audio(`../assets/bgm/${bgmList[Math.floor(Math.random() * 7)]}.ogg`);
+    bgm.loop = true;
+    bgm.play();
+}
 
 function playButtonDisabledSound(){
     btnDisabled_sound.pause();
